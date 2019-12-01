3655ee445d9c20389d2653a3b65c20adc45c8652
Ashwin Devaraj <ashwin.dev.pro@gmail.com>
2019-12-01 02:23:00 -0600

    created trivial 2nd network

--- nfsp/nn.py
+++ nfsp/nn.py
@@ -16,3 +16,17 @@ class DQN(nn.Module):
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
         return x
+
+class PG(nn.Module):
+
+    def __init__(self, dim=52*7+35+2):
+        super(PG, self).__init__()
+        self.fc1 = nn.Linear(dim, 300)
+        self.fc2 = nn.Linear(300, 200)
+        self.fc3 = nn.Linear(200, 52)
+    
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.softmax(self.fc3(x))
+        return x
