1efdce2ba4e1c61741072ee3d134dfcc8c151975
Ashwin Devaraj <ashwin.dev.pro@gmail.com>
2019-12-02 20:28:41 -0600

    changed card comparator to include trump suit information

--- contract_bridge/envs/bridge_trick_taking.py
+++ contract_bridge/envs/bridge_trick_taking.py
@@ -1,76 +1,7 @@
-import random
-from functools import total_ordering
-from itertools import product
-
 import gym
 from gym import spaces, utils
 from gym.error import InvalidAction
-
-@total_ordering
-class Card:
-    #12-14 is J, Q, K, A
-    ranks = (2,3,4,5,6,7,8,9,10,11,12,13,14)
-    suits = ('C','D','H','S')
-    
-    def __init__(self, rank, suit):
-        if rank in Card.ranks:
-            self.rank = rank
-        else:
-            raise Exception('The rank must be an integer between 2 and 14 inclusive.')
-
-        if suit in Card.suits:
-            self.suit = suit
-        else:
-            raise Exception('The suit must be \'C\', \'D\', \'H\', or \'S\'.')
-    
-    def __str__(self):
-        if (self.rank == 14):
-            rank = 'A'
-        elif (self.rank == 13):
-            rank = 'K'
-        elif (self.rank == 12):
-            rank = 'Q'
-        elif (self.rank == 11):
-            rank = 'J'
-        else:
-            rank = str (self.rank)
-        return rank + self.suit
-    
-    def __hash__(self):
-        return hash((self.rank, self.suit))
-    
-    def __eq__ (self, other):
-        return (self.rank == other.rank) and (self.suit == other.suit)
-    
-    def __ne__ (self, other):
-        return not self.__eq__(other)
-    
-    def __lt__ (self, other):
-        if self.suit != other.suit:
-            return self.suit < other.suit
-        else:
-            return self.rank < other.rank
-
-class Deck:
-    def __init__(self):
-        self.deck = list(map(lambda x: Card(rank=x[0], suit=x[1]), 
-                        product(Card.ranks, Card.suits)))
-        self.shuffle()
-    
-    def shuffle(self):
-        random.shuffle(self.deck)
-    
-    def is_empty(self):
-        return len(self.deck) == 0
-    
-    def deal(self):
-        if self.is_empty():
-            return None
-        else:
-            return self.deck.pop(0)
-    
-    def __str__(self):
-        return ', '.join(map(lambda x: str(x), self.deck))
+from .deck import Card, Deck
 
 class BridgeEnv(gym.Env):
     """
